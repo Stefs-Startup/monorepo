96837b20921735498459b5fddd584ed9e10e3340
Stefan Cross <sheldoncrosss@gmail.com>
2026-04-04 19:14:05 -0400

    chore(backstage): disable guest authentication in production

--- apps/backstage/packages/backend/src/index.ts
+++ apps/backstage/packages/backend/src/index.ts
@@ -26,7 +26,9 @@ backend.add(import('@backstage/plugin-techdocs-backend'));
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
-backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
+if (process.env.NODE_ENV !== 'production') {
+  backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
+}
 // See https://backstage.io/docs/auth/guest/provider
 backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 
